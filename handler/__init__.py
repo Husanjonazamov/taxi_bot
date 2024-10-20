018b74f3e0971b886575e21e4b1bc206a9ee4f58
Husanjonazamov <azamovhusanboy08@gmail.com>
2024-10-20 15:31:44 +0500

    change

--- handler/__init__.py
+++ handler/__init__.py
@@ -3,7 +3,6 @@ print('1. handler')
 from . import start
 from . import back
 from . import passenger
-from . import driver
-from . import chat
 from . import mail
+from . import driver
 from . import chat
--- handler/chat/handler.py
+++ handler/chat/handler.py
@@ -16,12 +16,12 @@ async def chat_handler_task(message: Message, state: FSMContext):
     Gurpada foydalanuvchilar xabariga javob beruvchi funksiya
     """
     
-    user_id=message.from_user.id
+    user_id = message.from_user.id
     print(user_id)
     
-    if message.text.lower().startswith("pochta"):
+    if message.text and message.text.lower().startswith("pochta"):
         username = message.from_user.username or "No username" 
-        mail=message.text
+        mail = message.text
         try:
             await bot.send_message(
                 chat_id=CHANNEL_ID,
@@ -30,15 +30,31 @@ async def chat_handler_task(message: Message, state: FSMContext):
                     mail=mail
                 ),
                 reply_markup=buttons.group_mail_success_admin(user_id)
-            )  
+            )
         except Exception as e:
             return
-    else:
-        await message.answer(texts.CHAT) 
+    # Boshqa xabarlar uchun javob bermaydi
 
+
+async def new_member_handler_task(message: Message):
+    """
+    Guruhga yangi foydalanuvchi qo'shilganda unga xabar yuboruvchi funksiya
+    """
+    for new_user in message.new_chat_members:
+        username = new_user.username or new_user.full_name or "No username"
+        # Yangi foydalanuvchiga xabar yuborish
+        await message.reply(
+            texts.CHAT.format(username=username)
+        )
 
 
 @dp.message_handler(content_types=['text'], state='*')
 async def chat_handler(message: Message, state: FSMContext):
     if message.chat.type in ['group', 'supergroup']:  
-        await create_task(chat_handler_task(message, state)) 
+        await create_task(chat_handler_task(message, state))
+
+
+@dp.message_handler(content_types=['new_chat_members'], state='*')
+async def new_member_handler(message: Message, state: FSMContext):
+    if message.chat.type in ['group', 'supergroup']:
+        await create_task(new_member_handler_task(message))
